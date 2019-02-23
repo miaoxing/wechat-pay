8c6bca36ca42694004b7fa4c4ec06a9c5cf3c278
twinh <twinhuang@qq.com>
2019-02-23 12:47:18 +0800

    refactoring: 移除bootprompt，直接更新bootbox到5.0

--- public/js/admin/wechat-qrcode-products.js
+++ public/js/admin/wechat-qrcode-products.js
@@ -1,5 +1,5 @@
 /* global Bloodhound */
-define(['plugins/product/js/numeric', 'plugins/app/js/bootbox/bootbox', 'plugins/app/libs/artTemplate/template.min'], function (numeric, bootbox, template) {
+define(['plugins/product/js/numeric', 'plugins/app/js/bootbox', 'plugins/app/libs/artTemplate/template.min'], function (numeric, bootbox, template) {
   var self = {};
 
   self.container = $('.product-qrcode-generator');
